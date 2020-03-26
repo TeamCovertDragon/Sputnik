803143c4e2fa8f160ca7c4ba5367ff8bb6e6b395
纪华裕 <8042833@qq.com>
2020-03-26 20:44:07 +0800

    feat(summary): add some summary (#6)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,24 +10,78 @@ module.exports = {
     sidebar: [
       {
         title: '序',
-        path:"/00-preface/",
+        path:"/00/",
         collapsable: true,
         children: [
-          '/00-preface/preface-0',
-          '/00-preface/preface-1'
+          '/00/preface-0',
+          '/00/preface-1'
         ]
       },
       {
         title: '预备知识',
-        path:"/01-preparing/",
+        path:"/01/",
         collapsable: true,
         children: [
-          '/01-preparing/java',
-          '/01-preparing/mdk',
-          '/01-preparing/gradle-ide',
-          '/01-preparing/mcp',
-          '/01-preparing/forge',
-          '/01-preparing/fg',
+          '/01/java',
+          '/01/mdk',
+          '/01/gradle-ide',
+          '/01/mcp',
+          '/01/forge',
+          '/01/fg',
+        ]
+      },
+      {
+        title: '入口',
+        path:"/02/",
+        collapsable: true,
+        children: [
+        ]
+      },
+      {
+        title: '事件',
+        path:"/03/",
+        collapsable: true,
+        children: [
+        ]
+      },
+      {
+        title: '注册物',
+        path:"/04/",
+        collapsable: true,
+        children: [
+          '/04/GameObject',
+          '/04/ForgeRegistryEntry',
+
+        ]
+      },
+      {
+        title: '简单模组',
+        path:"/05/",
+        collapsable: true,
+        children: [
+          '/05/item',
+          '/05/block',
+        ]
+      },
+      {
+        title: '物品进阶',
+        path:"/06/",
+        collapsable: true,
+        children: [
+        ]
+      },
+      {
+        title: '方块进阶',
+        path:"/07/",
+        collapsable: true,
+        children: [
+        ]
+      },
+      {
+        title: '方块实体',
+        path:"/08/",
+        collapsable: true,
+        children: [
         ]
       },
       {
--- docs/README.md
+++ docs/README.md
@@ -1,27 +1,25 @@
 # Sputnik
 
-Sputnik 是 [Harbinger][ref-1] 的续作，为新版 Minecraft 而生。
+你现在看到的是新一代的 Minecraft 模组开发教程————Sputnik
 
-```java
-package com.example.examplemod;
+## Sputnik 是什么
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+Sputnik 是 [Harbinger][ref-1] 的续作，为新版 Minecraft 而生。与 Harbinger 不同，Sputnik不仅基于更新的版本，1.15.2 还更加考虑读者的感受。
 
-@Mod("examplemod")
-public class ExampleMod {
 
-    public ExampleMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-    }
+## 为什么是 1.15.2
 
-    private void setup(final FMLCommonSetupEvent event) {
-        System.out.println("Hello, Forge");
-    }
+Sputnik 的出现是历史的选择，从 forge 不再支持 1.12.2 开始，1.12.2 的教程就注定是即将落伍的，为了顺应时代的浪潮，Sputnik 选择了 1.15.2 的 Minecraft。
 
-}
+## 我要怎么做
 
-```
+Sputnik 分为数个知识点，每个知识点分为两部分：
+
+- 上半部分：抛弃细节，主要讲解概念以及大致流程。
+- 下半部分：主要讲解细节，例如具体代码实现。
+
+我们觉得这样能使读者更好的接受这个教程。
+
+如果你愿意开始学习，请拿出你的决心，一起和我写下这一行行代码，构建这绚丽的**我的世界**。
 
 [ref-1]: https://harbinger.covertdragon.team
